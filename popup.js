61b5a521088ea0d20fe3df24224d28319fbd8d4a
Zhengyang Song <songzy_thu@163.com>
2022-06-23 20:43:33 +0800

    Extract info from HoldingLists.

--- popup.js
+++ popup.js
@@ -16,11 +16,26 @@ extractInfo.addEventListener("click", async () => {
 // current page
 // TODO(songzy): complete this function.
 function extractInfoFromPage() {
+    let result = '';
+
     let copy_info = document.querySelector(".copy_info");
+    result += copy_info.innerText + '\n'
+
+    let holdingslists = document.querySelectorAll(".holdingslist");
+    row_content = ''
+    for (let i = 0; i < holdingslists.length; i++) {
+        row_content += holdingslists[i].innerText + '\t';
+        if (i % 4 == 3) {
+            result += row_content + '\n'
+            row_content = ''
+        }
+    }
 
     let textarea = document.createElement('textarea');
+    textarea.style.width = '750px';
+    textarea.style.height = '350px';
     document.body.parentNode.insertBefore(textarea, document.body);
-    textarea.value = copy_info.innerText;
+    textarea.value = result;
     textarea.select();
     document.execCommand('copy');
 }
